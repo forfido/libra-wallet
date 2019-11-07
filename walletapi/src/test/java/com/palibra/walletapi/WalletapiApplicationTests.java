b7f84fc3ce2ebc127575fb814ba707e843697763
yhk <younghok@gmail.com>
2019-11-07 14:39:34 +0900

    리브라 블럭제인 잔액 조회 테스트 성공

--- walletapi/src/test/java/com/palibra/walletapi/WalletapiApplicationTests.java
+++ walletapi/src/test/java/com/palibra/walletapi/WalletapiApplicationTests.java
@@ -5,13 +5,15 @@ import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import java.security.Security;
+
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class WalletapiApplicationTests {
 
-//    @Test
-//    public void contextLoads() {
-//
-//    }
+    @Test
+    public void contextLoads() {
+
+    }
 
 }
--- walletapi/src/test/java/com/palibra/walletapi/domain/libraaccount/LibraAccountServiceTest.java
+++ walletapi/src/test/java/com/palibra/walletapi/domain/libraaccount/LibraAccountServiceTest.java
@@ -1,26 +1,59 @@
 package com.palibra.walletapi.domain.libraaccount;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.palibra.walletapi.domain.auth.SignUpRequest;
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
 
+import java.security.Security;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@AutoConfigureMockMvc
 public class LibraAccountServiceTest {
+
+    @Autowired
+    MockMvc mockMvc;
 
     @Autowired
     LibraAccountService libraAccountService;
 
-    @Test
-    public void findAccount() {
-    }
+    @Autowired
+    ObjectMapper objectMapper;
 
-    @Test
-    public void testFindAccount() {
+    @Before
+    public void setUp() throws Exception {
+        Security.addProvider(new org.bouncycastle.jce.provider.BouncyCastleProvider());
+
+        SignUpRequest param = new SignUpRequest();
+        param.setEmail("ahin@palibra.net");
+        param.setPassword("123123");
+        param.setName("아인");
+
+        mockMvc.perform(post("/auth/signUp")
+                    .contentType(MediaType.APPLICATION_JSON_UTF8)
+                    .accept(MediaType.APPLICATION_JSON)
+                    .content(objectMapper.writeValueAsString(param)))
+                .andDo(print())
+                .andExpect(jsonPath("success").exists());
     }
 
     @Test
     public void getBalanceTest() {
 
-        LibraAccount account = libraAccountService.findAccount(2L);
+        LibraAccount account = libraAccountService.findAccount(1L);
 
         Long balance = libraAccountService.getBalance(account.getLibraAddressToString());
 
