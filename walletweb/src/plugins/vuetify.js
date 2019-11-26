175ce72b2045eb8e09d4b01aa3dc32a0ffce4c7d
bigone <dikim@payletter.com>
2019-11-26 17:53:24 +0900

    vuetify icon 추가 작업

--- walletweb/src/plugins/vuetify.js
+++ walletweb/src/plugins/vuetify.js
@@ -1,8 +1,12 @@
 
+import "@fortawesome/fontawesome-free/css/all.css";
+import "@mdi/font/css/materialdesignicons.css";
+import "material-design-icons-iconfont/dist/material-design-icons.css";
+
 import Vue from "vue";
 import Vuetify from "vuetify/lib";
 import "vuetify/src/stylus/app.styl";
 
 Vue.use(Vuetify, {
-  iconfont: 'md',
+  iconfont: "md" || "fa" || "mdi"
 });
