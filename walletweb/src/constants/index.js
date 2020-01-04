9a3149eb3da4beee617d66c64087008e1183975d
YoungHo Kim <younghok@gmail.com>
2020-01-04 11:30:22 +0900

    프론트에 환경변수 분리할 필요 있음

--- walletweb/src/constants/index.js
+++ walletweb/src/constants/index.js
@@ -3,8 +3,8 @@ export default {
   USER_INFO: "UserInfo",
   AUTHORIZTION: "Authorization",        // Http HeaderName
   HTTPTIMEOUT: 10000,
-  ENDPOINT: "http://localhost:8090",            // BackEndPoint URL
-  FRONTPOINT: "http://localhost:3000",            // FrontEndPoint URL
+  ENDPOINT: "http://wallet-api.palibra.net",            // BackEndPoint URL   local: localhost:8090
+  FRONTPOINT: "http://www.palibra.net",        // FrontEndPoint URL  local: localhost:3000
   MICORLIBRARATE: 1000000,
   LIBRAROOTADDRESS: "000000000000000000000000000000000000000000000000000000000a550c18",
 }
