5480945574cc4664f08df95b234df472a9bcb141
YoungHo Kim <younghok@gmail.com>
2020-01-05 10:12:15 +0900

    aws 배포 완료

--- walletapi/src/main/java/com/palibra/walletapi/security/oauth2/OAuth2AuthenticationSuccessHandler.java
+++ walletapi/src/main/java/com/palibra/walletapi/security/oauth2/OAuth2AuthenticationSuccessHandler.java
@@ -55,7 +55,7 @@ public class OAuth2AuthenticationSuccessHandler extends SimpleUrlAuthenticationS
                 .map(Cookie::getValue);
 
         if(redirectUri.isPresent() && !isAuthorizedRedirectUri(redirectUri.get())) {
-            throw new BadRequestException("Sorry! We've got an Unauthorized Redirect URI and can't proceed with the authentication");
+            throw new BadRequestException("Sorry! We've got an Unauthorized Redirect URI and can't proceed with the authentication(" + redirectUri.get()+")");
         }
 
         String targetUrl = redirectUri.orElse(getDefaultTargetUrl());
@@ -80,11 +80,8 @@ public class OAuth2AuthenticationSuccessHandler extends SimpleUrlAuthenticationS
                 .anyMatch(authorizedRedirectUri -> {
                     // Only validate host and port. Let the clients use different paths if they want to
                     URI authorizedURI = URI.create(authorizedRedirectUri);
-                    if(authorizedURI.getHost().equalsIgnoreCase(clientRedirectUri.getHost())
-                            && authorizedURI.getPort() == clientRedirectUri.getPort()) {
-                        return true;
-                    }
-                    return false;
+                    return authorizedURI.getHost().equalsIgnoreCase(clientRedirectUri.getHost())
+                            && authorizedURI.getPort() == clientRedirectUri.getPort();
                 });
     }
 }
